ee9db4d987d943d8d5aa5d63e34e6284afab39a1
alexspaceman <alexp@jolera.com>
2015-12-13 21:00:17 -0500

    more socket.on

--- app.js
+++ app.js
@@ -2,19 +2,37 @@ var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
+var user = ''
+var room = ''
+
+var users = []
+var rooms = []
+
 app.get('/', (req, res) => {
   res.sendfile('test.html')
 })
 
 io.on('connection', (socket) => {
-  console.log('connectedddd')
-  socket.on('disconnet', () => {
+  console.log('connected')
+  socket.on('disconnect', () => {
     console.log('disconnected')
   })
 
-  socket.on(`yoo`, () =>
-    socket.broadcast.emit(`yo`, { message: `yo` })
+  socket.on(`createUser`, (username) =>
+    user = username
+    users.push(username)
+    socket.broadcast.emit({ users })
   )
+
+  socket.on(`createRoom`, (roomname) => {
+    room = roomname
+    rooms.push(roomname)
+    socket.broadcast.emit({ rooms })
+  })
+
+  socket.on(`joinRoom`, (roomname) => {
+    // 
+  })
 })
 
 http.listen(8000, () => {
